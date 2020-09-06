d935c8cc74e6f7f691e93f0241b26a9c86404ee8
Amanda de Moura <5531103+dmdemoura@users.noreply.github.com>
2020-09-06 15:53:04 -0300

    Implementando preferencia com file picker.

--- Novo/app/src/main/java/one/demouraLima/fwallpaper/SettingsActivity.kt
+++ Novo/app/src/main/java/one/demouraLima/fwallpaper/SettingsActivity.kt
@@ -1,24 +1,43 @@
 package one.demouraLima.fwallpaper
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 class SettingsActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
         supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            val pref = findPreference<Preference>("test")
+
+            pref?.onPreferenceClickListener = Preference.OnPreferenceClickListener { preference: Preference ->
+                registerForActivityResult(ActivityResultContracts.OpenDocumentTree()) { uri: Uri? ->
+                    if (uri != null)
+                    {
+                        val prefStorage = PreferenceManager.getDefaultSharedPreferences(preference.context)
+                        val prefEditor = prefStorage.edit()
+                        prefEditor.putString(preference.key, uri.toString())
+                        prefEditor.apply()
+                    }
+                }
+
+                true
+            }
         }
     }
 }
